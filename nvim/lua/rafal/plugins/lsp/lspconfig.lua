1da5dfb29fddf1b9b0559a9b44e0a81c370b2995
Rafał Leżanko <rafal.lezanko@zowie.ai>
2025-08-06 13:34:23 +0200

    lsp upgraded

--- nvim/lua/rafal/plugins/lsp/lspconfig.lua
+++ nvim/lua/rafal/plugins/lsp/lspconfig.lua
@@ -5,13 +5,11 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
+		"williamboman/mason-lspconfig.nvim", -- Dodano, aby zapewnić automatyczne włączanie serwerów
 	},
 	config = function()
 		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
 
 		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
@@ -74,44 +72,41 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
+		-- Konfiguracja serwerów bezpośrednio
+		-- mason-lspconfig automatycznie włączy te serwery, jeśli są zainstalowane przez Mason
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
 					},
-				})
-			end,
+					completion = {
+						callSnippet = "Replace",
+					},
+				},
+			},
 		})
+
+		lspconfig["graphql"].setup({
+			capabilities = capabilities,
+			filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+		})
+
+		-- Domyślny handler jest teraz niepotrzebny, ponieważ mason-lspconfig automatycznie konfiguruje serwery.
+		-- Wystarczy, że upewnisz się, że są na liście 'ensure_installed' w pliku mason.lua
+		-- Opcjonalnie, możesz skonfigurować inne serwery tutaj
+		lspconfig["pyright"].setup({ capabilities = capabilities })
+		lspconfig["jdtls"].setup({ capabilities = capabilities })
+		lspconfig["gopls"].setup({ capabilities = capabilities })
+		lspconfig["gradle_ls"].setup({ capabilities = capabilities })
+		lspconfig["kotlin_language_server"].setup({ capabilities = capabilities })
 	end,
 }
--- nvim/lua/rafal/plugins/lsp/mason.lua
+++ nvim/lua/rafal/plugins/lsp/mason.lua
@@ -8,9 +8,7 @@ return {
 		-- import mason
 		local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
+		-- import mason-tool-installer
 		local mason_tool_installer = require("mason-tool-installer")
 
 		-- enable mason and configure icons
@@ -21,19 +19,6 @@ return {
 					package_pending = "➜",
 					package_uninstalled = "✗",
 				},
-			},
-		})
-
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"lua_ls",
-				"graphql",
-				"pyright",
-				"jdtls",
-				"gopls",
-				"gradle_ls",
-				"kotlin_language_server",
 			},
 		})
 
